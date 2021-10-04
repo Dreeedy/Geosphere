72fa078275937197948a668c45aecfbc02e8f983
Dreeedy <dreeedyone@gmail.com>
2021-10-04 22:39:58 +0300

    feat: базовая реализация GeographicService

---
+++ Geosphere/GeographicService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geosphere
+{
+    abstract class GeographicService
+    {
+        abstract public string Search(in SearchQuery searchQuery, ref IHttpClient httpClient);
+    }
+}
---
+++ Geosphere/NominatimGeographicService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geosphere
+{
+    class NominatimGeographicService : GeographicService
+    {
+        public override string Search(in SearchQuery searchQuery, ref IHttpClient httpClient)
+        {
+            string result;
+            httpClient.UpdateUrl(in searchQuery);
+
+            result = httpClient.GetContent();
+
+            return result;
+        }
+    }
+}
