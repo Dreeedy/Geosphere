2c4f9745109eb49d9d4f306acc3db4db08a942c8
Dreeedy <dreeedyone@gmail.com>
2021-10-04 16:46:47 +0300

    Add project files.

---
+++ Geosphere/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Geosphere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
