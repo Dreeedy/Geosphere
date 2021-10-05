5f2c3ff64547c6cfbd40bb18f4b1829562458775
Dreeedy <dreeedyone@gmail.com>
2021-10-05 11:17:26 +0300

    refactor: новые комментарии

--- Geosphere/HttpGeographicClient.cs
+++ Geosphere/HttpGeographicClient.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Net.Http;
-using System.Text;
+﻿using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Geosphere
@@ -13,28 +8,25 @@ namespace Geosphere
         private static readonly HttpClient _httpClient;
         private static string _ulr;
 
+        /// <summary>
+        /// Статический конструктор обеспечивает единоразовое создание HttpClient при первом обращении
+        /// </summary>
         static HttpGeographicClient()
         {
             _httpClient = new HttpClient();
         }
 
-        private string UpdateUrl(in SearchQuery searchQuery)
-        {
-            string url = $"https://nominatim.openstreetmap.org/search?" +
-                $"q={searchQuery.GetAddress()}" +
-                $"&polygon_threshold={searchQuery.GetPolygonSimplification()}" +
-                $"&format=json" +
-                $"&polygon_geojson=1";
-
-            return url;
-        }
-
+        /// <summary>
+        /// Метод обращается к географическому сервису и возвращает результат
+        /// </summary>
+        /// <param name="searchQuery"></param>
+        /// <returns></returns>
         public string GetContent(in SearchQuery searchQuery)
         {
             _ulr = UpdateUrl(in searchQuery);
 
-            var task = GetContentAsync(_httpClient);
-            task.Wait();
+            var task = GetContentAsync(_httpClient); // Асинхронный вызов метода
+            task.Wait();// Ожидание окончания работы асинхронного метода
 
             string result = task.Result;
 
@@ -46,17 +38,42 @@ namespace Geosphere
             return result;
         }
 
+        /// <summary>
+        /// Метод обновляет новыми данными url сервиса, к которому проихсодит обрщение
+        /// </summary>
+        /// <param name="searchQuery"></param>
+        /// <returns></returns>
+        private string UpdateUrl(in SearchQuery searchQuery)
+        {
+            string url = $"https://nominatim.openstreetmap.org/search?" +
+                $"q={searchQuery.GetAddress()}" +
+                $"&polygon_threshold={searchQuery.GetPolygonSimplification()}" +
+                $"&format=json" +
+                $"&polygon_geojson=1";
+
+            return url;
+        }
+
+        /// <summary>
+        /// Метод эмулирует "отпечатки" браузера и выполняет обрщание к географическому сервису
+        /// </summary>
+        /// <param name="client"></param>
+        /// <returns></returns>
         private async Task<string> GetContentAsync(HttpClient client)
         {
             ConsoleHandler.WriteCyan("[1/4] Выполняет запрос к географическому сервису \"nominatim.openstreetmap.org\"... ");
 
             try
             {
+                // Эмуляция отпечаток браузера
                 client.DefaultRequestHeaders.TryAddWithoutValidation("Accept", "application/json");
                 client.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:19.0) Gecko/20100101 Firefox/19.0");
 
+                // Обращение к географическому сервису и получкние ответа
                 HttpResponseMessage response = await client.GetAsync(_ulr);
                 response.EnsureSuccessStatusCode();
+
+                // Чтение ответа от сервиса
                 string responseBody = await response.Content.ReadAsStringAsync();
 
                 return responseBody;
